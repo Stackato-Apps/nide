ab8d0629936142715ea7e9570780257135f7cb4a
Billy Tat <billyt@activestate.com>
2014-08-11 14:57:35 -0700

    Do not attempt to launch the default browser

--- prepare_project.sh
+++ prepare_project.sh
@@ -1,5 +1,5 @@
 if [ -d $STACKATO_FILESYSTEM/.nide ]; then
-  node main.js listen $STACKATO_FILESYSTEM
+  node main.js listen $STACKATO_FILESYSTEM -b
 else
-  node main.js init $STACKATO_FILESYSTEM
+  node main.js init $STACKATO_FILESYSTEM -b
 fi
